cb3f72b4ddd92b6f576446ade161f41249412966
Fran Aragón <franaragon@gransliving.com>
2021-12-02 18:52:50 +0100

    Add icons in header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,9 @@
 //Componente que renderizará el header de la web
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faBell, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faSearch, faBell } from '@fortawesome/fontawesome-free-solid';
 
 const Header = (props) => {
     return (
@@ -9,14 +12,14 @@ const Header = (props) => {
                 <img src='/logo.png' alt='logo' className='w-14 cursor-pointer' />
                 <div className='ml-4 flex cursor-pointer'>
                     <p className='text-white text-xs mt-1'>Menú</p>
-                    {/* <FontAwesomeIcon className='text-white ml-2 mt-1' icon={faCaretDown} /> */}
+                    <FontAwesomeIcon className='text-white ml-2 mt-1' icon={faCaretDown} />
                 </div>
             </div>
             <div className='flex'>
-                {/* <FontAwesomeIcon className='text-white ml-2 text-xs mt-2 mr-6 cursor-pointer' icon={faSearch} />
-                <FontAwesomeIcon className='text-white ml-2 text-xs mt-2 mr-16 cursor-pointer' icon={faBell} /> */}
+                <FontAwesomeIcon className='text-white ml-2 text-xs mt-2 mr-6 cursor-pointer' icon={faSearch} />
+                <FontAwesomeIcon className='text-white ml-2 text-xs mt-2 mr-16 cursor-pointer' icon={faBell} />
                 <div className='group'>
-                    <img src='/image-user.png' alt='image user' className='absolute cursor-pointer w-9 right-4 top-2 rounded-md' />
+                    <img src='/image-user.png' alt='image user' className='absolute cursor-pointer w-9 right-4 top-4 rounded-md' />
                     {/* <div className=' invisible group-hover:visible '>HOLA AMIGOS DE YOUTUVE</div> */}
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home() {
 
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      {/* <footer className="flex items-center justify-center w-full h-24 border-t">
 
-      </footer>
+      </footer> */}
     </div>
   )
 }
